13434fc4bd888814b7cb1c36e5602294f7dca155
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-21 20:57:52 -0300

    feat: criei uma rota filmes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const filmesRouter = require("./routes/filmesRoutes");
+
+app.use("/filmes", filmesRouter);
+
 app.use(express.json());
 app.use(cors());
 module.exports = app;
