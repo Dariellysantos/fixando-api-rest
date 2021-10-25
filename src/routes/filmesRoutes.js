072ab745d22644578c634b08c2e2bf77699e0353
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-24 23:10:27 -0300

    fix: ordenei as rotas

--- src/routes/filmesRoutes.js
+++ src/routes/filmesRoutes.js
@@ -9,8 +9,8 @@ router.get("/list", controller.getAll);
 router.get("/id/:id", controller.getById);
 router.get("/title", controller.getByTitle);
 router.post("/criar", controller.createMovie);
-router.patch("/updateTitle/:id", controller.updateTitle);
 router.put("/update/:id", controller.update);
+router.patch("/updateTitle/:id", controller.updateTitle);
 router.patch("/update/:id", controller.updateMovieBody);
 router.delete("/delete/:id", controller.deleteMovie);
 
